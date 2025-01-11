dfee66b057f0e90950aaa74aefa56f0eb2a00417
jakob-i-am <0xjakob@gmail.com>
2025-01-12 03:29:40 +1100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    text_node = TextNode('this is a text node', 'bold', 'http://localhost:8080')
+    print(text_node)
+
+if __name__ == '__main__':
+    main()
